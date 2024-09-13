877fdfe955e38cb51a6e7629ea45add3d03b3795
BenOno <bentoki.1213@gmail.com>
2024-09-13 16:14:19 -0700

    finish

--- exercise1.js
+++ exercise1.js
@@ -1,15 +1,18 @@
 // Exercise 1: Create a Prototype
-// Instructions: 
+// Instructions:
 // 1. Create a constructor function called "Vehicle" that takes "make" and "model" as arguments.
 // 2. Define a method called "startEngine" on the prototype of Vehicle that logs "The [make] [model] engine is starting."
 
-
+function Vehicle(make, model) {
+  this._make = make
+  this._model = model
+}
 
 // YOUR CODE HERE
-
-
-
+Vehicle.prototype.startEngine = function () {
+  console.log(`The ${this._make} ${this._model} engine is starting.`)
+}
 
 // Test the Vehicle prototype
-const myCar = new Vehicle("Toyota", "Corolla");
-myCar.startEngine();  // Output: The Toyota Corolla engine is starting.
+const myCar = new Vehicle("Toyota", "Corolla")
+myCar.startEngine() // Output: The Toyota Corolla engine is starting.
